1afa65c8fec56c8642304a8206ca0a2696dbb267
runner.mei <runner.mei@gmail.com>
2012-11-22 22:08:23 +0800

    add validator

---
+++ store/validate_test.go
@@ -0,0 +1,226 @@
+package store
+
+import (
+	"regexp"
+	"testing"
+	"time"
+)
+
+func TestDate(t *testing.T) {
+
+	value1, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:11")
+	value2, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:12")
+	value3, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:13")
+	value4, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:14")
+	value5, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:15")
+	value6, _ := time.Parse("2006-01-02 15:04:05", "2009-10-11 12:12:16")
+
+	var validator DateValidator
+
+	assertTrue := func(value interface{}) {
+		if ok, err := validator.validate(value); !ok {
+			t.Errorf("test Date failed, %s", err.Error())
+		}
+	}
+	assertFalse := func(value interface{}) {
+		if ok, _ := validator.validate(value); ok {
+			t.Errorf("test Date failed")
+		}
+	}
+
+	assertTrue(value1)
+	assertTrue(value5)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = value5
+	assertTrue(value1)
+	assertTrue(value5)
+	assertFalse(value6)
+
+	validator.HasMax = false
+	validator.MaxValue = value5
+	validator.HasMin = true
+	validator.MinValue = value2
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = value5
+	validator.HasMin = true
+	validator.MinValue = value2
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value3)
+	assertTrue(value4)
+	assertTrue(value5)
+	assertFalse(value6)
+}
+
+func TestInteger(t *testing.T) {
+	var value1 int = 1
+	var value2 int = 2
+	var value3 int = 3
+	var value4 int = 4
+	var value5 int = 5
+	var value6 int = 6
+
+	var validator IntegerValidator
+
+	assertTrue := func(value interface{}) {
+		if ok, err := validator.validate(value); !ok {
+			t.Errorf("test integer failed, %s", err.Error())
+		}
+	}
+	assertFalse := func(value interface{}) {
+		if ok, _ := validator.validate(value); ok {
+			t.Errorf("test integer failed")
+		}
+	}
+
+	assertTrue(value1)
+	assertTrue(value5)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = int64(value5)
+	assertTrue(value1)
+	assertTrue(value5)
+	assertFalse(value6)
+
+	validator.HasMax = false
+	validator.MaxValue = int64(value5)
+	validator.HasMin = true
+	validator.MinValue = int64(value2)
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = int64(value5)
+	validator.HasMin = true
+	validator.MinValue = int64(value2)
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value3)
+	assertTrue(value4)
+	assertTrue(value5)
+	assertFalse(value6)
+}
+
+func TestDouble(t *testing.T) {
+
+	var value1 float32 = 1.0
+	var value2 float32 = 2.0
+	var value3 float32 = 3.0
+	var value4 float32 = 4.0
+	var value5 float32 = 5.0
+	var value6 float32 = 6.0
+
+	var validator DecimalValidator
+
+	assertTrue := func(value interface{}) {
+		if ok, err := validator.validate(value); !ok {
+			t.Errorf("test float failed, %s", err.Error())
+		}
+	}
+	assertFalse := func(value interface{}) {
+		if ok, _ := validator.validate(value); ok {
+			t.Errorf("test float failed")
+		}
+	}
+
+	assertTrue(value1)
+	assertTrue(value5)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = float64(value5)
+	assertTrue(value1)
+	assertTrue(value5)
+	assertFalse(value6)
+
+	validator.HasMax = false
+	validator.MaxValue = float64(value5)
+	validator.HasMin = true
+	validator.MinValue = float64(value2)
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value6)
+
+	validator.HasMax = true
+	validator.MaxValue = float64(value5)
+	validator.HasMin = true
+	validator.MinValue = float64(value2)
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value3)
+	assertTrue(value4)
+	assertTrue(value5)
+	assertFalse(value6)
+}
+
+func TestString(t *testing.T) {
+
+	var value1 string = "aaa1"
+	var value2 string = "aaaa2"
+	var value3 string = "aaaaa3"
+	var value4 string = "aaaaaa4"
+	var value5 string = "aaaaaaa5"
+	var value6 string = "aaaaaaaa6"
+
+	var validator StringValidator
+
+	assertTrue := func(value interface{}) {
+		if ok, err := validator.validate(value); !ok {
+			t.Errorf("test string failed, %s", err.Error())
+		}
+	}
+	assertFalse := func(value interface{}) {
+		if ok, _ := validator.validate(value); ok {
+			t.Errorf("test string failed")
+		}
+	}
+
+	validator.MaxLength = -1
+	validator.MinLength = -1
+	assertTrue(value1)
+	assertTrue(value5)
+	assertTrue(value6)
+
+	validator.MaxLength = len(value5)
+	validator.MinLength = -1
+	assertTrue(value1)
+	assertTrue(value5)
+	assertFalse(value6)
+
+	validator.MaxLength = -1
+	validator.MinLength = len(value2)
+
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value6)
+
+	validator.MaxLength = len(value5)
+	validator.MinLength = len(value2)
+	assertFalse(value1)
+	assertTrue(value2)
+	assertTrue(value3)
+	assertTrue(value4)
+	assertTrue(value5)
+	assertFalse(value6)
+
+	validator.MaxLength = -1
+	validator.MinLength = -1
+
+	validator.Pattern, _ = regexp.Compile("a.*")
+	assertFalse("ddd")
+	assertTrue("aaa")
+}
---
+++ store/validator.go
@@ -0,0 +1,186 @@
+package store
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type Validatable interface {
+	validate(value interface{}) (bool, error)
+}
+
+type StringValidator struct {
+	MinLength, MaxLength int
+	Pattern              *regexp.Regexp
+}
+
+func (self *StringValidator) validate(obj interface{}) (bool, error) {
+	value, ok := obj.(string)
+	if !ok {
+		return false, errors.New("syntex error")
+	}
+
+	if 0 <= self.MinLength && self.MinLength > len(value) {
+		return false, errors.New("length of '" + value + "' is less " + strconv.Itoa(self.MinLength))
+	}
+
+	if 0 <= self.MaxLength && self.MaxLength < len(value) {
+		return false, errors.New("length of '" + value + "' is greate " + strconv.Itoa(self.MaxLength))
+	}
+
+	if nil != self.Pattern {
+		if !self.Pattern.MatchString(value) {
+			return false, errors.New("'" + value + "' is not match '" + self.Pattern.String() + "'")
+		}
+	}
+	return true, nil
+}
+
+type PhysicalAddressValidator struct{}
+
+func (self *PhysicalAddressValidator) validate(obj interface{}) (bool, error) {
+	value, ok := obj.(string)
+	if !ok {
+		return false, errors.New("syntex error")
+	}
+
+	if _, err := net.ParseMAC(value); nil != err {
+		return false, errors.New("syntex error, " + err.Error())
+	}
+	return true, nil
+}
+
+type IPAddressValidator struct{}
+
+func (self *IPAddressValidator) validate(obj interface{}) (bool, error) {
+	value, ok := obj.(string)
+	if !ok {
+		return false, errors.New("syntex error")
+	}
+	if nil == net.ParseIP(value) {
+		return false, errors.New("syntex error")
+	}
+	return true, nil
+}
+
+type IntegerValidator struct {
+	HasMin, HasMax     bool
+	MinValue, MaxValue int64
+}
+
+func (self *IntegerValidator) validate(obj interface{}) (bool, error) {
+	var value int64
+	switch v := obj.(type) {
+	case int:
+		value = int64(v)
+	case int8:
+		value = int64(v)
+	case int16:
+		value = int64(v)
+	case int32:
+		value = int64(v)
+	case int64:
+		value = int64(v)
+	default:
+		return false, errors.New("syntex error")
+	}
+
+	if self.HasMin && self.MinValue > value {
+		return false, fmt.Errorf("'%d' is less minValue '%d'", value, self.MinValue)
+	}
+
+	if self.HasMax && self.MaxValue < value {
+		return false, fmt.Errorf("'%d' is greate maxValue '%d'", value, self.MaxValue)
+	}
+
+	return true, nil
+}
+
+type EnumerationValidator struct {
+	Values []interface{}
+}
+
+func (self *EnumerationValidator) validate(obj interface{}) (bool, error) {
+	var found bool = false
+	for v := range self.Values {
+		if v == obj {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return false, fmt.Errorf("enum is not contains %v", obj)
+	}
+	return true, nil
+}
+
+type DecimalValidator struct {
+	HasMin, HasMax     bool
+	MinValue, MaxValue float64
+}
+
+func (self *DecimalValidator) validate(obj interface{}) (bool, error) {
+	var value float64
+	switch v := obj.(type) {
+	case uint:
+		value = float64(v)
+	case uint8:
+		value = float64(v)
+	case uint16:
+		value = float64(v)
+	case uint32:
+		value = float64(v)
+	case uint64:
+		value = float64(v)
+	case int:
+		value = float64(v)
+	case int8:
+		value = float64(v)
+	case int16:
+		value = float64(v)
+	case int32:
+		value = float64(v)
+	case int64:
+		value = float64(v)
+	case float32:
+		value = float64(v)
+	case float64:
+		value = float64(v)
+	default:
+		return false, errors.New("syntex error")
+	}
+
+	if self.HasMin && self.MinValue > value {
+		return false, fmt.Errorf("'%f' is less minValue '%f'", value, self.MinValue)
+	}
+
+	if self.HasMax && self.MaxValue < value {
+		return false, fmt.Errorf("'%f' is greate maxValue '%f'", value, self.MaxValue)
+	}
+	return true, nil
+}
+
+type DateValidator struct {
+	HasMin, HasMax     bool
+	MinValue, MaxValue time.Time
+}
+
+func (self *DateValidator) validate(obj interface{}) (bool, error) {
+	value, ok := obj.(time.Time)
+	if !ok {
+		return false, errors.New("syntex error")
+	}
+
+	if self.HasMin && self.MinValue.After(value) {
+		return false, fmt.Errorf("'%s' is less minValue '%s'", value.String(), self.MinValue.String())
+	}
+
+	if self.HasMax && self.MaxValue.Before(value) {
+		return false, fmt.Errorf("'%s' is greate maxValue '%s'", value.String(), self.MaxValue.String())
+	}
+	return true, nil
+}

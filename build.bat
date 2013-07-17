f3f3c2dc5810a9630e3f9ffd6d00019d7ded8043
gziliang@aliyun.com <gziliang@aliyun.com>
2013-07-17 14:17:25 +0800

    auto get work directory

--- build.bat
+++ build.bat
@@ -6,27 +6,27 @@ go get github.com/lib/pq
 go get github.com/mattn/go-sqlite3
 go get code.google.com/p/mahonia
 
-set ENGINE_PATH=C:\git_repository\oschina\tpt_nm\src\engine\
-set PUBLISH_PATH=C:\git_repository\oschina\tpt_nm\publish\
+set WD=%~dp0
+set SOURCE_PATH=%WD%..\
+set PUBLISH_PATH=%WD%..\..\..\publish\
 
-cd %ENGINE_PATH%src\data_store\ds
+cd %SOURCE_PATH%src\data_store\ds
 del "*.exe"
 go build
+
 @if errorlevel 1 goto failed
-copy "ds.exe"  %PUBLISH_PATH%tpt_ds.exe
-@if errorlevel 1 goto failed
-xcopy /Y /S /E %ENGINE_PATH%src\data_store\etc\*   %PUBLISH_PATH%lib\models
+xcopy /Y /S /E %SOURCE_PATH%src\data_store\etc\*   %PUBLISH_PATH%lib\models\
 @if errorlevel 1 goto failed
 
 
-cd %ENGINE_PATH%src\sampling\sampling
+cd %SOURCE_PATH%src\sampling\sampling
 del "*.exe"
 go build
 @if errorlevel 1 goto failed
 copy "sampling.exe"  %PUBLISH_PATH%bin\tpt_sampling.exe
 
 
-cd %ENGINE_PATH%src\poller\poller
+cd %SOURCE_PATH%src\poller\poller
 del "*.exe"
 go build
 @if errorlevel 1 goto failed
@@ -34,7 +34,7 @@ copy "poller.exe" %PUBLISH_PATH%bin\tpt_poller.exe
 @if errorlevel 1 goto failed
 
 
-cd %ENGINE_PATH%src\carrier\carrier
+cd %SOURCE_PATH%src\carrier\carrier
 del "*.exe"
 go build
 @if errorlevel 1 goto failed
@@ -42,18 +42,18 @@ copy "carrier.exe" %PUBLISH_PATH%bin\tpt_carrier.exe
 @if errorlevel 1 goto failed
 
 
-REM cd %ENGINE_PATH%src\bridge\discovery_tools
+REM cd %SOURCE_PATH%src\bridge\discovery_tools
 REM go build
 REM @if errorlevel 1 goto failed
-REM copy "%ENGINE_PATH%src\bridge\discovery_tools\discovery_tools.exe" %~dp0\bin\discovery_tools.exe
+REM copy "%SOURCE_PATH%src\bridge\discovery_tools\discovery_tools.exe" %~dp0\bin\discovery_tools.exe
 REM @if errorlevel 1 goto failed
 
 cd %~dp0
-copy "%ENGINE_PATH%src\lua_binding\lib\lua52.dll" %PUBLISH_PATH%bin\lua52.dll
+copy "%SOURCE_PATH%src\lua_binding\lib\lua52.dll" %PUBLISH_PATH%bin\lua52.dll
 @if errorlevel 1 goto failed
-copy "%ENGINE_PATH%src\lua_binding\lib\cjson.dll" %PUBLISH_PATH%bin\cjson.dll
+copy "%SOURCE_PATH%src\lua_binding\lib\cjson.dll" %PUBLISH_PATH%bin\cjson.dll
 @if errorlevel 1 goto failed
-xcopy /Y /S /E "%ENGINE_PATH%src\lua_binding\microlight\*" %PUBLISH_PATH%\lib\microlight
+xcopy /Y /S /E "%SOURCE_PATH%src\lua_binding\microlight\*" %PUBLISH_PATH%\lib\microlight\
 @if errorlevel 1 goto failed
 
 @goto :eof

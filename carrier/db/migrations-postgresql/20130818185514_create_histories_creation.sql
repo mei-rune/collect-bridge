a2d0e0236c23e87df069df5bc2c972d3a6c60269
runner.mei <runner.mei@gmail.com>
2013-08-20 18:47:36 +0800

    unremove db trigger from migration script

--- carrier/db/migrations-postgresql/20130818185514_create_histories_creation.sql
+++ carrier/db/migrations-postgresql/20130818185514_create_histories_creation.sql
@@ -1,6 +1,6 @@
 -- +goose Up
-SELECT tpt_histories_partition_creation( '2010-01-01', '2028-01-01' );
+SELECT tpt_histories_partition_creation( '2013-01-01', '2014-01-01' );
 
 
 -- +goose Down
-SELECT tpt_histories_partition_deletion( '2010-01-01', '2028-01-01' );
+SELECT tpt_histories_partition_deletion( '2013-01-01', '2014-01-01' );

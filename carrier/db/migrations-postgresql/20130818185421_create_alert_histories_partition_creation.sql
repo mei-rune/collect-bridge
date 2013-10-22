1459876566d74179f25a4bbc41bd7ea367e1b970
runner.mei <runner.mei@gmail.com>
2013-10-22 15:56:47 +0800

    refact tpt_alert_histories_partition_creation and tpt_histories_partition_creation

--- carrier/db/migrations-postgresql/20130818185421_create_alert_histories_partition_creation.sql
+++ carrier/db/migrations-postgresql/20130818185421_create_alert_histories_partition_creation.sql
@@ -2,62 +2,40 @@
 CREATE OR REPLACE FUNCTION tpt_alert_histories_partition_creation( DATE, DATE )
 returns void AS $$
 DECLARE
-  create_query text;                 -- declare create_query variable
-  drop_triggerd_index_query text;    -- declare drop_index_query variable
-  create_triggerd_index_query text;  -- declare create_index_query variable
-  drop_mo_index_query text;          -- declare drop_mo_index_query variable
-  create_mo_index_query text;        -- declare create_mo_index_query variable
-  drop_action_index_query text;      -- declare drop_action_index_query variable
-  create_action_index_query text;    -- declare create_action_index_query variable
+  t DATE;     -- the time
+  tname text; -- the table name
 BEGIN
-  FOR create_query, drop_triggerd_index_query, create_triggerd_index_query,
-  drop_mo_index_query, create_mo_index_query,
-  drop_action_index_query, create_action_index_query IN SELECT
-      'CREATE TABLE IF NOT EXISTS tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || ' ( CHECK( triggered_at >= timestamp '''
-      || TO_CHAR( d, 'YYYY-MM-DD 00:00:00' )
-      || ''' AND triggered_at < timestamp '''
-      || TO_CHAR( d + INTERVAL '1 month', 'YYYY-MM-DD 00:00:00' )
-      || ''' ) ) inherits ( tpt_alert_histories );',
-      'DROP INDEX  IF EXISTS tpt_alert_histories_'
-      ||TO_CHAR( d, 'YYYY_MM' )
-      ||'_triggered_at_idx;',
-      'CREATE INDEX tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || '_triggered_at_idx ON tpt_alert_histories_' 
-      || TO_CHAR( d, 'YYYY_MM' )
-      || ' ( triggered_at );',
-      'DROP INDEX IF EXISTS tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || '_mo_id_idx;',
-      'CREATE INDEX tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || '_mo_id_idx ON tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || ' USING btree (managed_type COLLATE pg_catalog."default", managed_id);',
-      'DROP INDEX IF EXISTS tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || '_action_id_idx;',
-      'CREATE INDEX tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || '_action_id_idx ON tpt_alert_histories_'
-      || TO_CHAR( d, 'YYYY_MM' )
-      || ' USING btree (action_id);'
+  FOR t, tname IN SELECT d, 'tpt_alert_histories_' || TO_CHAR( d, 'YYYY_MM' )
     FROM generate_series( $1, $2, '1 month' ) AS d
   LOOP
-    EXECUTE create_query;        -- excute create table
-    EXECUTE drop_triggerd_index_query;    -- drop index on triggered_at
-    EXECUTE create_triggerd_index_query;  -- create index on triggered_at
-    EXECUTE drop_mo_index_query;          -- drop index on mo
-    EXECUTE create_mo_index_query;        -- create index on mo
-    EXECUTE drop_action_index_query;      -- drop index on action
-    EXECUTE create_action_index_query;    -- create index on action
+    IF NOT EXISTS (SELECT * FROM pg_catalog.pg_tables WHERE tablename = tname) THEN
+      EXECUTE 'CREATE TABLE '
+      || tname
+      || ' ( CHECK( triggered_at >= timestamp '''
+      || TO_CHAR( t, 'YYYY-MM-DD 00:00:00' )
+      || ''' AND triggered_at < timestamp '''
+      || TO_CHAR( t + INTERVAL '1 month', 'YYYY-MM-DD 00:00:00' )
+      || ''' ) ) inherits ( tpt_alert_histories );'
+      || 'CREATE INDEX '
+      || tname
+      || '_triggered_at_idx ON ' 
+      || tname
+      || ' ( triggered_at );'
+      || 'CREATE INDEX '
+      || tname
+      || '_mo_id_idx ON '
+      || tname
+      || ' USING btree (managed_type COLLATE pg_catalog."default", managed_id);'
+      || 'CREATE INDEX '
+      || tname
+      || '_action_id_idx ON '
+      || tname
+      || ' USING btree (action_id);'; -- create table 
+      END IF; -- IF END
   END LOOP;  -- LOOP END
 END;         -- FUNCTION END
 $$
 language plpgsql;
-
 
 
 CREATE OR REPLACE FUNCTION tpt_alert_histories_partition_deletion( DATE, DATE )
